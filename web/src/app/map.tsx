e374c80874f9f274411565882190b2715ce1217e
moogiikat <monkhoo1997ts@gmail.com>
2024-09-07 09:52:07 +0900

    refactor

--- web/src/app/map.tsx
+++ web/src/app/map.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { MapPinIcon, PhoneCallIcon, MailIcon } from "lucide-react";
 
 export const GoogleMapEmbed: React.FC = () => {
   return (
-    <div className="grid md:grid-cols-2 gap-x-5">
+    <div className="grid md:grid-cols-2 gap-x-5 gap-y-5">
       <iframe
         width="100%"
         height="100%"
@@ -10,18 +11,24 @@ export const GoogleMapEmbed: React.FC = () => {
         src="https://maps.google.com/maps?q=Workmaster%20LLC,%20Энхтайвны%20Өргөн%20Чөлөө,%20ウランバートル&t=&z=13&ie=UTF8&iwloc=&output=embed"
         title="Google Map"
       ></iframe>
-      <div className="flex flex-col gap-y-2">
-        <p>
+      <div className="flex flex-col gap-y-5">
+        <p className="flex items-center gap-x-2">
+          <MapPinIcon className="w-10" />
           {
-            " Хаяг: Баянзүрх дүүрэг, 3-р хороо, Токиогийн гудамж, MILLENIUM PLAZA 1305 тоот, Ulaanbaatar, Mongolia"
+            "MILLENIUM PLAZA 1305 тоот"
           }
         </p>
-        <p>
-          Утас: <a href="tel:+97699232628">+97699232628</a>
+        <p className="flex items-center">
+          <PhoneCallIcon className="w-10" />
+          <a href="tel:+97699232628" className="ml-1">
+            +97699232628
+          </a>
         </p>
-        <p>
-          И-мэйл:
-          <a href="mailto:mailto:info@workmaster.mn">{" info@workmaster.mn"}</a>
+        <p className="flex items-center">
+          <MailIcon className="w-10" />
+          <a href="mailto:info@workmaster.mn" className="ml-1">
+            info@workmaster.mn
+          </a>
         </p>
       </div>
     </div>
